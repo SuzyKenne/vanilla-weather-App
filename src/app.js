8b72d13d083b332a52acd6dd1897db5043bbf335
SuzyKenne <suzyndatewo@gmail.com>
2023-09-14 00:49:30 +0000

    added the forecast day

--- src/app.js
+++ src/app.js
@@ -13,6 +13,32 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let days = ["wed", "Thu", "Fri", "Sat", "Sun"];
+    days.forEach(function(day) {
+      forecastHTML = forecastHTML + 
+     `
+        <div class="col-2">
+            <div class="weather-forecast-date">${day}</div>
+            <img
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png"
+                alt=""
+                width="42"
+            />
+            <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">18°</span>
+                <span class="weather-forecast-temperature-min">12°</span>
+            </div>
+        </div>`;
+        
+    }
+    );
+    forecastHTML =forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
 
 
 function displayTemperature(response) {
@@ -83,3 +109,5 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+displayForecast();
