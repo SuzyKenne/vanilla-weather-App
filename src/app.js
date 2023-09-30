b924ca41d22c98eaa5349155c2897452d4a142f8
SuzyKenne <suzyndatewo@gmail.com>
2023-09-30 01:33:31 +0000

    project transfer to React project

--- src/app.js
+++ src/app.js
@@ -1,137 +1,68 @@
-function formatDate(timestamp) {
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    if(hours<10){
-        hours = `0${hours}`
-    }
-    let minutes = date.getMinutes();
-    if(minutes<10){
-        minutes = `0${minutes}`
-    }
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satursday"];
-    let day = days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
-}
+import React from "react";
+import "./style.css";
 
-function formatDay(timestamp) {
-    let date = new Date(timestamp * 1000);
-    let days = date.getDay();
-    let day = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    temperature: 19,
+    date: "Tuesday 10:00",
+    description: "Cloudy",
+    imgUrl: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII=",
+    humidity: 80,
+    wind: 10
+  };
 
-    return day[days];
-}
-
-function displayForecast(response) {
-    console.log (response.data.daily);
-    let forecast = response.data.daily;
-    let forecastElement = document.querySelector("#forecast");
-
-    let forecastHTML = `<div class="row">`;
-    forecast.forEach(function (forecastDay, index) {
-        if (index<6) {
-        forecastHTML =
-          forecastHTML +
-          `
-        <div class="col-2">
-          <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
-          <img
-          src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png"
-            alt=""
-            width="42"
-          />
-          <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temperature.maximum)}° </span>
-            <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temperature.minimum)}° </span>
+  return (
+    <div className="Weather-app">
+      <form className="mb-3">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city.."
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
-    `;
-        }
-      }
-    )
-  
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
+      </form>
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
-  
-
-
-function getForecast(city) {
-    console.log(city);
-    let Key = "430030o1b1545b06bdd75tb02bbad91f";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${Key}&units=metric`;
-    axios.get(apiUrl).then(displayForecast);
-    console.log(apiUrl);
-}
-
-function displayTemperature(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city");
-    let humidityElement = document.querySelector("#humidity");
-    let pressureElement = document.querySelector("#pressure");
-    let windElement = document.querySelector("#wind");
-    let descriptionElement = document.querySelector("#description");
-    let dateElement = document.querySelector("#date");
-    let iconElement = document.querySelector("#icon");
-
-    celsiusTemperature = response.data.temperature.current;
-
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-    cityElement.innerHTML = response.data.city;
-    humidityElement.innerHTML = response.data.temperature.humidity;
-    pressureElement.innerHTML = response.data.temperature.pressure;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    descriptionElement.innerHTML = response.data.condition.description;
-    dateElement.innerHTML = formatDate(response.data.time * 1000);
-    iconElement.setAttribute ("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`); 
-
-    getForecast(response.data.coordinates);
-}
-
-function search(city) {
-    let Key = "430030o1b1545b06bdd75tb02bbad91f";
-    let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${Key}&units=metric`;
-    console.log(apiUrl);
-
-    axios.get(apiUrl).then(displayTemperature);
-}
-
-function handleSubmit(event) {
-    event.preventDefault();
-    let cityInputElement = document.querySelector("#city-input");
-    search(cityInputElement.value);
-}
-
-
-function displayFahrenheitTemperature(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    //remove the active class from the celsius element
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let fahrenheint = (celsiusTemperature + 9)/ 5 + 32;
-    
-    temperatureElement.innerHTML = Math.round(fahrenheint);
-}
-
-
-function displayCelsiusTemperature(event) {
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-displayForecast();
